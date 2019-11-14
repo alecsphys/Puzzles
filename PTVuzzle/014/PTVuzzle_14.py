3991eb6718671293ed09f73a0c26329474f8f446
Alessandro.Attanasi <alexapta@gmail.com>
2019-11-14 14:32:52 +0100

    implemented the first algorithm solution. Btw fixing an error into the pseudocode

--- PTVuzzle/014/PTVuzzle_14.py
+++ PTVuzzle/014/PTVuzzle_14.py
@@ -1,5 +1,27 @@
+import copy
+import time
+
+
 def main():
     puzzleQuestion(14)
+
+    maxGridSize = 10
+    timingList1 = []
+    timingList2 = []
+
+    for g in range(9, maxGridSize + 1):
+        t0 = time.process_time()
+        puzzle14solution1(g, printPaths=False)
+        t1 = time.process_time()
+        timingList1.append((t1-t0))
+    print("Process time [sec] = ", timingList1, sum(timingList1))
+
+    for g in range(9, maxGridSize + 1):
+        t0 = time.process_time()
+        #puzzle14solution2(g, printPaths=False)
+        t1 = time.process_time()
+        timingList2.append((t1-t0))
+    print("Process time [sec] = ", timingList2, sum(timingList2))
 
 
 def puzzleQuestion(puzzleNumber):
@@ -11,6 +33,68 @@ def puzzleQuestion(puzzleNumber):
           "totally above the diagonal connecting (0,0) with (N,N)? Can you design an efficient (in time and space)\n"
           "algorithm to fully enumerate all those paths?")
     print("===========================================================================================================")
+
+
+def puzzle14solution1(gridSize, printPaths=True):
+    print("Grid Size = ", gridSize)
+
+    p = Path(gridSize)
+    p.updateNorthCounter()
+    p.updateSequence(1)
+    pathList = [p]
+
+    pathFinishedCounter = 0
+    while len(pathList) > 0:
+        pathNorth = copy.deepcopy(pathList.pop())
+        pathEast = copy.deepcopy(pathNorth)
+        if pathNorth.expandNorth() is not None:
+            pathList.append(pathNorth)
+        [pathEastFinished, pathEast] = pathEast.expandEast(printIfFinished=printPaths)
+        if pathEastFinished:
+            pathFinishedCounter += 1
+        else:
+            if pathEast is not None:
+                pathList.append(pathEast)
+    print("Total number of paths = ", pathFinishedCounter)
+
+
+class Path:
+    def __init__(self, gridSize):
+        self.gridSize = gridSize
+        self.sequence = []
+        self.northCounter = 0
+
+    def updateSequence(self, value):
+        self.sequence.append(value)
+
+    def updateNorthCounter(self):
+        self.northCounter += 1
+
+    def decodeSequence(self):
+        stringfinal = ""
+        for i in self.sequence:
+            stringfinal += ("N" if i == 1 else "E")
+        return stringfinal
+
+    def expandNorth(self):
+        newPath = None
+        if self.northCounter < self.gridSize:
+            self.updateNorthCounter()
+            self.updateSequence(1)
+            newPath = self
+        return newPath
+
+    def expandEast(self, printIfFinished=True):
+        newPath = None
+        pathFinished = False
+        if len(self.sequence) < 2 * self.northCounter:
+            self.updateSequence(0)
+            newPath = self
+        elif len(self.sequence) == 2 * self.gridSize:
+            pathFinished = True
+            if printIfFinished:
+                print(self.decodeSequence())
+        return [pathFinished, newPath]
 
 
 if __name__ == "__main__":
